75d5bff34f813cdaef41b7251014b61e4e36b9e1
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2016-02-21 10:38:46 -0600

    Se agregan los modulos de soporte para la programacion web, morgan, body-parser etc...

--- server.js
+++ server.js
@@ -4,14 +4,14 @@
 // Importing express framework
 var express = require('express');
 // Getting configuration
-//var config = require('./server/configure);
+var config = require('./server/configure');
 // Getting app instance
 var app = express();
 // Setting some configuration
 app.set('port', process.env.PORT || 3000);
 app.set('ip', process.env.IP || '127.0.0.1');
 app.set('views', __dirname + '/views');
-//app = config(app);
+app = config(app);
 
 // Routing
 app.get('/', function(req, res){
---
+++ server/configure.js
@@ -0,0 +1,31 @@
+/* global __dirname */
+// importing dependencies
+var path = require('path'),
+    //routes = require('./routes'),
+    exphdb = require('express-handlebars'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    morgan = require('morgan'),
+    methodOverride = require('method-override'),
+    errorHandler = require('errorhandler');
+
+module.exports = function(app){
+    // Inserting midleware
+    app.use(morgan('dev'));
+    app.use(bodyParser.urlencoded({'extended': true}));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+    app.use(cookieParser('alugn-valor-secreto-aqui'));
+    
+    // Moving the routes to router folder
+    //routes(app);
+    
+    // Static midleware is put at the end
+    app.use('/public/',
+    express.static(path.join(__dirname, '../public')));
+    if('development' === app.get('env')){
+        app.use(errorHandler());
+    }
+    return app;
+};
