91434b0758f6e6af240d904fc7be1aacc81648bf
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2016-02-20 22:44:23 -0600

    Se crea el hola mundo de la app

---
+++ server.js
@@ -0,0 +1,24 @@
+/* global process */
+/* global __dirname */
+
+// Importing express framework
+var express = require('express');
+// Getting configuration
+//var config = require('./server/configure);
+// Getting app instance
+var app = express();
+// Setting some configuration
+app.set('port', process.env.PORT || 3000);
+app.set('ip', process.env.IP || '127.0.0.1');
+app.set('views', __dirname + '/views');
+//app = config(app);
+
+// Routing
+app.get('/', function(req, res){
+    res.send('Hola ITGAM');
+});
+// Starting Server
+app.listen(app.get('port'), app.get('ip'), function(){
+    console.log("> Server listenig @http://%s:%s",
+    app.get('ip'), app.get('port'));
+});
