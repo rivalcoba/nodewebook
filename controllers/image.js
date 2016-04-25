9812d4235cfe8f6d1fdce6defc5d4f67bb991ad7
rivalcoba <ivan.rivalcoba@gail.com>
2016-04-24 22:10:00 -0500

    Falla en el modelo

--- controllers/image.js
+++ controllers/image.js
@@ -48,25 +48,44 @@ module.exports = {
             for(var i=0; i < 6; i++){
                 imgUrl += possible.charAt(Math.floor(Math.random() * possible.length));
             }
-            // creating the storing path            
-            var tempPath = req.files[0].path,
-                ext = path.extname(req.files[0].originalname).toLowerCase(),
-                targetPath = path.resolve('./public/upload/' + imgUrl + ext);
-                console.log(">> ext: " + ext);
-                console.log(">> targetPath: %s", targetPath);
-            if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
-                fs.rename(tempPath, targetPath, function(err){
-                    if(err) throw err;
-                    res.redirect('/images/' + imgUrl);
-                });
-            }else{
-                // unlink borra el archivo
-                fs.unlink(tempPath, function(err){
-                    if(err) throw err;
-                    console.log(">> DELTED FILE: %s", tempPath);
-                    res.status(500).json({error: 'Solo los archivos de imagenes estan permitidos'});
-                });
-            }
+            Models.Image.find({filename: imgUrl},(err, images)=>{
+                if(images.length > 0){
+                    // An image with the same name
+                    // of the random genereated was found
+                    saveImage();
+                }else{
+                    // Do all the existing work
+                    // creating the storing path            
+                    var tempPath = req.files[0].path,
+                        ext = path.extname(req.files[0].originalname).toLowerCase(),
+                        targetPath = path.resolve('./public/upload/' + imgUrl + ext);
+                        console.log(">> ext: " + ext);
+                        console.log(">> targetPath: %s", targetPath);
+                    if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
+                        fs.rename(tempPath, targetPath, function(err){
+                            if(err) throw err;
+                            // Create a new image model, and populate it
+                            var newImg = new Models.Image({
+                                title: req.body.title,
+                                description: req.body.description,
+                                filename: imgUrl + ext
+                            });
+                            // Save the new image
+                            newImg.save((err, image)=>{
+                                console.log(`> Succefully inserted image: ${image.filename}`);
+                                res.redirect('/images/' + imgUrl);
+                            });
+                        });
+                    }else{
+                        // unlink borra el archivo
+                        fs.unlink(tempPath, function(err){
+                            if(err) throw err;
+                            console.log(">> DELTED FILE: %s", tempPath);
+                            res.status(500).json({error: 'Solo los archivos de imagenes estan permitidos'});
+                        });
+                    }
+                }
+            });        
         };
         saveImage();
         //res.status(200).json(req.files[0]);
