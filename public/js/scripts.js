d4f43d883e0e3befbfb3a66c85d1a6f935bfdcc8
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2016-08-07 20:57:46 -0500

    Sweet alert funcionando

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -20,20 +20,35 @@ $(function(){
     $('#btn-delete').on('click', function(event){
         event.preventDefault();
         var $this = $(this);
-        var remove = confirm('Esta seguro de que desea eliminar esta imágen?');
-        if(remove){
-            var imgId = $(this).data('id');
-            // Making the ajax call
-            $.ajax({
-                url: "/images/"+imgId,
-                type: 'DELETE'
-            }).done(function(result){
-                if(result){
-                    $this.removeClass('btn-danger').addClass('btn-success');
-                    $this.find('i').removeClass('fa fa-times').addClass('fa fa-check');
-                    $this.append('<span> Borrada!</span>');
-                }
-            });
-        }
+        swal({
+            title: "¿Estas seguro?",
+            text: "Esta operación es irreversible.",
+            type:"warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "¡Si, Borralo!",
+            cancelButtonText: "¡No, cancela la operación!",
+            closeOnConfirm: false,
+            closeOnCancel: false
+        },function(remove){
+            if(remove){
+                var imgId = $this.data('id');
+                console.log("> imgId " + imgId);
+                //Making the ajax call
+                $.ajax({
+                    url: "/images/"+imgId,
+                    type: 'DELETE'
+                }).done(function(result){
+                    if(result){
+                        swal("¡Borrada!", "La imagen ha sido borrada", "success");
+                        $this.removeClass('btn-danger').addClass('btn-success');
+                        $this.find('i').removeClass('fa fa-times').addClass('fa fa-check');
+                        $this.append('<span> Borrada!</span>');
+                    }
+                });
+            }else{
+                swal("Cancelado", "Se ha cancelado el borrado de la imágen", "error");
+            }
+        });
     });
 });
